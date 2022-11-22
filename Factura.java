17932013bd162230ac141e023fce9e9c3cf3d32c
midenceza <jose.midence14840@est.uca.edu.ni>
2022-11-22 15:34:09 -0600

    Generé el código de java

---
+++ Factura.java
@@ -0,0 +1,64 @@
+package Package1;
+
+
+/**
+ * <ol>
+ * 	<li></li>
+ * </ol>
+ * @author MARIO
+ * @version 1.0
+ * @created 22-nov.-2022 03:32:36 p. m.
+ */
+public class Factura {
+
+	private int codigo;
+	private String fecha;
+	private Cliente m_Cliente;
+	private Producto m_Producto;
+	private Vendedor m_Vendedor;
+
+	public Factura(){
+
+	}
+	public void finalize(){
+
+	}
+
+	/**
+	 * 
+	 * @param Producto
+	 */
+	public void agregarProducto(Producto Producto){
+
+	}
+
+	/**
+	 * 
+	 * @param precio
+	 * @param iva
+	 */
+	public double aplicarIVA(double precio, double iva){
+		return 0;
+	}
+
+	/**
+	 * 
+	 * @param precio
+	 * @param totalProductos
+	 */
+	public double calcMonto(double precio, double totalProductos){
+		return 0;
+	}
+
+	/**
+	 * 
+	 * @param Producto
+	 */
+	public double calcTotal(List<Producto> Producto){
+		return 0;
+	}
+
+	public factura(){
+
+	}
+}//end Factura
